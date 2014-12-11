76e47a35374e9fe04423617d9592356d2f6f4d7d
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-11 10:27:34 -0500

    Add gpg keys now required by rvm

---
+++ rvm-keys/recipes/default.rb
@@ -0,0 +1,10 @@
+# rvm installer now requires gpg keys be present if gpg
+# is present on a system
+
+# this should get added to fnichol/chef-rvm at some point, see
+# https://github.com/fnichol/chef-rvm/issues/278
+
+execute 'Adding gpg keys' do
+	command 'gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 BF04FF17'
+	only_id 'which gpg'
+end
