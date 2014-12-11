dbc0acb101a94a2472e7eabb2cbf6ca36b9b0be4
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-11 17:34:12 -0500

    Install system and user rubies to rvm and set as default

---
+++ rvm-config/recipes/rubies.rb
@@ -0,0 +1,21 @@
+bash "setup system rubies" do
+  # use mount command and S3 bucket for azn lnx 2014.09 ruby build
+  # also faster than building
+  code "/usr/local/rvm/bin/rvm mount -r https://s3.amazonaws.com/StelligentLabsResources/rvm/rubies/amazon/ruby-1.9.3-p551.tar.bz2 --verify-downloads 2"
+end
+
+bash "setup user rubies" do
+  # use mount command and S3 bucket for azn lnx 2014.09 ruby build
+  # also faster than building
+  code "rvm mount -r https://s3.amazonaws.com/StelligentLabsResources/rvm/rubies/amazon/ruby-1.9.3-p551.tar.bz2 --verify-downloads 2"
+  user 'jenkins'
+end
+
+rvm_default_ruby 'system' do
+	ruby_string '1.9.3'
+end
+
+rvm_default_ruby 'jenkins'	do
+	ruby_string '1.9.3'
+	user 'jenkins'
+end
