8c58e02056d7292afb616a429f2d7d304e51dfb7
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-12 16:54:58 -0500

    work around default user ruby brokenness

--- rvm-config/recipes/rubies.rb
+++ rvm-config/recipes/rubies.rb
@@ -17,6 +17,7 @@ bash "setup user rubies" do
   . .profile
   env
   ./.rvm/bin/rvm mount -r https://s3.amazonaws.com/StelligentLabsResources/rvm/rubies/amazon/ruby-1.9.3-p551.tar.bz2 --verify-downloads 2
+  ./.rvm/bin/rvm --default use 1.9.3
   EOH
   user 'jenkins'
 end
@@ -25,7 +26,10 @@ rvm_default_ruby 'system' do
 	ruby_string '1.9.3'
 end
 
+# seems broken with non-system user
+if false
 rvm_default_ruby 'jenkins'	do
 	ruby_string '1.9.3'
 	user 'jenkins'
+end
 end
