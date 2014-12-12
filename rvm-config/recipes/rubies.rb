d252efa9268cc9d47b62167f72cacadc21fe71ba
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-12 15:22:28 -0500

    Use normal multiline syntax, debug env

--- rvm-config/recipes/rubies.rb
+++ rvm-config/recipes/rubies.rb
@@ -8,7 +8,12 @@ bash "setup user rubies" do
   # use mount command and S3 bucket for azn lnx 2014.09 ruby build
   # also faster than building
   cwd '/var/lib/jenkins'
-  code ". .rvmrc;. .profile; ./.rvm/bin/rvm mount -r https://s3.amazonaws.com/StelligentLabsResources/rvm/rubies/amazon/ruby-1.9.3-p551.tar.bz2 --verify-downloads 2"
+  code <<-EOH
+  . .rvmrc
+  . .profile
+  env
+  ./.rvm/bin/rvm mount -r https://s3.amazonaws.com/StelligentLabsResources/rvm/rubies/amazon/ruby-1.9.3-p551.tar.bz2 --verify-downloads 2"
+  EOH
   user 'jenkins'
 end
 
