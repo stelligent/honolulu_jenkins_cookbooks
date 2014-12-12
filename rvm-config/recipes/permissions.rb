7fb2bd551a745e6cefc174448642bd046418b188
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-12 11:01:22 -0500

    Set permissions on aws dirs to allow file_cache_path for unpriv users

---
+++ rvm-config/recipes/permissions.rb
@@ -0,0 +1,12 @@
+# AWS OpsWorks has restrictive permissions on the /var/lib/aws and /var/lib/aws/opsworks
+# directories which makes it impossible for an unpriv user to read scripts in
+# /var/lib/aws/opsworks/cache.stage2/ to execute them, which breaks rvm user installs
+# see provider_rvm_installation.rb in the rvm cookbook for implementation details
+
+file '/var/lib/aws' do
+	mode '0751'
+end
+
+file '/var/lib/aws/opsworks' do
+	mode '0751'
+end
