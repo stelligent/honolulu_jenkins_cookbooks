03beca12e0afc1c2d395af8b42b3d3424ddeabb0
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-12 19:17:59 -0500

    don't try to install deps

--- jenkins-configuration/recipes/install_plugins.rb
+++ jenkins-configuration/recipes/install_plugins.rb
@@ -4,5 +4,6 @@
 node['jenkins']['server']['plugins'].each do |plugin|
   jenkins_plugin plugin['name'] do
     version plugin['version'] unless plugin['version'].nil?
+    install_deps false
   end
 end
