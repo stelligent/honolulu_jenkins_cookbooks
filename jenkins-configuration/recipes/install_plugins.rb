f1902e8c6f6c4dcebd277e7436f6e86f158dba1a
Jeff Bachte <jeffrey.bachtel@stelligent.com>
2014-12-15 13:56:10 -0500

    logging unneeded

--- jenkins-configuration/recipes/install_plugins.rb
+++ jenkins-configuration/recipes/install_plugins.rb
@@ -15,13 +15,6 @@ node['jenkins']['server']['plugins'].each do |plugin|
   	group 'jenkins'
   	mode '0644'
   end
-  ruby_block "Log for #{plugin['name']}" do
-  	block do
-  		dirll = `/bin/ls -al /var/lib/jenkins/plugins`
-  		Chef::Log.warn("Directory for #{plugin['name']}: #{dirll}")
-  	end
-  	action :create
-  end
 end
 
 # wait 10 seconds for jenkins to stop responding on its
@@ -31,11 +24,3 @@ execute 'wait for jenkins to restart' do
 end
 
 jenkins_command 'version'
-
-ruby_block "Log for post-install_plugins recipe" do
-  block do
-    dirll = `/bin/ls -al /var/lib/jenkins/plugins`
-    Chef::Log.warn("Directory for post-install_plugins plugins: #{dirll}")
-  end
-  action :create
-end
