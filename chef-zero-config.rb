a7773193f37bbc654612508b4ba6355e072632a5
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2015-01-09 16:27:19 -0500

    extra line from paste

--- chef-zero-config.rb
+++ chef-zero-config.rb
@@ -1,4 +1,3 @@
-chef-zero-config.rb
 chef_zero.enabled true
 cookbook_path ['/opt/aws/opsworks/current','/opt/aws/opsworks/current/berks-cookbooks']
 json_attribs '/opt/aws/opsworks/current/custom.json'
