9c12cf47890eacb169db0ee65eac96a57bc4aaee
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2015-01-09 16:06:35 -0500

    Configuration file for chef-zero via cfn

---
+++ chef-zero-config.rb
@@ -0,0 +1,4 @@
+chef-zero-config.rb
+chef_zero.enabled true
+cookbook_path ['/opt/aws/opsworks/current','/opt/aws/opsworks/current/berks-cookbooks']
+json_attribs '/opt/aws/opsworks/current/custom.json'
